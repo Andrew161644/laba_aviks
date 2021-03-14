daa89cad51d177b4a66c9bc5fea29d1a55f03e79
KhafizovFidan <hafizov.fr00@gmail.com>
2021-03-14 16:14:33 +0500

    Добавить ссылки на все страницы

--- rest/api/handlers/mainpage.go
+++ rest/api/handlers/mainpage.go
@@ -9,7 +9,7 @@ import (
 
 func (app *Injection) HelloPageHandler(w http.ResponseWriter, r *http.Request) {
 	data := views.ViewData{
-		Title: "Проект",
+		Title: "Главная",
 	}
 	tmpl, _ := template.ParseFiles("../static/main.html")
 	err := tmpl.Execute(w, data)
@@ -23,6 +23,42 @@ func (app *Injection) AboutHandler(w http.ResponseWriter, r *http.Request) {
 		Title: "О нас",
 	}
 	tmpl, _ := template.ParseFiles("../static/about.html")
+	err := tmpl.Execute(w, data)
+	if err != nil {
+		fmt.Println(err)
+		fmt.Fprintf(w, "Error")
+	}
+}
+
+func (app *Injection) DevelopersHandler(w http.ResponseWriter, r *http.Request) {
+	data := views.ViewData{
+		Title: "Об участниках",
+	}
+	tmpl, _ := template.ParseFiles("../static/members.html")
+	err := tmpl.Execute(w, data)
+	if err != nil {
+		fmt.Println(err)
+		fmt.Fprintf(w, "Error")
+	}
+}
+
+func (app *Injection) NewsHandler(w http.ResponseWriter, r *http.Request) {
+	data := views.ViewData{
+		Title: "Новости",
+	}
+	tmpl, _ := template.ParseFiles("../static/news.html")
+	err := tmpl.Execute(w, data)
+	if err != nil {
+		fmt.Println(err)
+		fmt.Fprintf(w, "Error")
+	}
+}
+
+func (app *Injection) FindDevHandler(w http.ResponseWriter, r *http.Request) {
+	data := views.ViewData{
+		Title: "Резюме",
+	}
+	tmpl, _ := template.ParseFiles("../static/resume.html")
 	err := tmpl.Execute(w, data)
 	if err != nil {
 		fmt.Println(err)
--- rest/api/routes/routes.go
+++ rest/api/routes/routes.go
@@ -6,4 +6,7 @@ import "github.com/Andrew161644/avicks_laba/api/handlers"
 func AddRoutes(app handlers.Injection) {
 	http.HandleFunc("/main_page", app.HelloPageHandler)
 	http.HandleFunc("/about", app.AboutHandler)
+	http.HandleFunc("/developers", app.DevelopersHandler)
+	http.HandleFunc("/news", app.NewsHandler)
+	http.HandleFunc("/find_dev", app.FindDevHandler)
 }
