c7989caf4b79f542c270d26385763fb8936d2956
KhafizovFidan <hafizov.fr00@gmail.com>
2021-04-06 01:12:43 +0500

    Добавить провайдер для таблицы years

--- rest/api/database/models/year.go
+++ rest/api/database/models/year.go
@@ -1,7 +1,10 @@
 package models
 
 type Year struct {
-	Id          int    `json:"id"`
+	ID          int    `json:"id"`
 	Name        string `json:"name"`
 	Description string `json:"description"`
+}
+type YearList struct {
+	Years []Year `json:"years"`
 }
